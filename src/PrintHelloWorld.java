18c645b9e7e61f68ada02a7f7e17397560b5251b
Bwire Phabiano <bwirephabiano939@gmail.com>
2019-09-04 20:58:58 +0300

    Initial commit

---
+++ src/PrintHelloWorld.java
@@ -0,0 +1,21 @@
+//language syntax
+
+// Access modifier Public, Protected, Default, Private
+
+public class PrintHelloWorld {
+//curly braces {}
+//semi colon ;
+//bracket ()
+//period or dot .
+//array []
+//data types i.e String, int, boolean, double
+//data structures array
+//variables
+//for loop
+
+    public static void main (String[] args) {
+        for (int i=0; i<10; i++) {
+            System.out.println(i + ": Hello World!");
+        }
+    }
+}
